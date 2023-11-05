f8f035909a925398585d0241158d9a386cdb5040
JordyV23 <jordyvargas0123@gmail.com>
2023-11-05 09:54:54 -0600

    Commit inicial

---
+++ read-data/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace read_data
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
